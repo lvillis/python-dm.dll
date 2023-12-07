35951a0f002458c2127afec894ddd4128e698f4f
null <56720445+lvillis@users.noreply.github.com>
2023-12-07 20:26:45 +0800

    chore(deps): bump python from 3.9 to 3.12

--- setup.py
+++ setup.py
@@ -36,6 +36,9 @@ def main():
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
+            'Programming Language :: Python :: 3.11',
+            'Programming Language :: Python :: 3.12',
         ],
         packages=find_packages(),
         install_requires=[
